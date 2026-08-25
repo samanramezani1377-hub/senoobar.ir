f679d6cdc68e202ced52df6c6b49057ccff57377
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-08-25 23:39:30 +0330

    Load WooCommerce shipping display helper

--- functions.php
+++ functions.php
@@ -29,6 +29,7 @@ require_once SENOOBAR_DIR . '/inc/wishlist-page-setup.php';
 require_once SENOOBAR_DIR . '/inc/legal-pages-setup.php';
 require_once SENOOBAR_DIR . '/inc/account.php';
 require_once SENOOBAR_DIR . '/inc/password-change.php';
+require_once SENOOBAR_DIR . '/inc/shipping-display.php';
 require_once SENOOBAR_DIR . '/inc/wishlist.php';
 require_once SENOOBAR_DIR . '/inc/otp-login.php';
 require_once SENOOBAR_DIR . '/inc/otp-register.php';
